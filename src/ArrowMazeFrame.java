8df7c033572b1e1355ad5f4ea81c97cd23aa3f63
harlan.howe <harlan.howe@kinkaid.org>
2024-02-10 15:08:07 -0600

    Added find1Path button. Added status label Refactored "Execute" to "Find All paths"

--- src/ArrowMazeFrame.java
+++ src/ArrowMazeFrame.java
@@ -6,14 +6,14 @@ import java.awt.event.ActionListener;
 public class ArrowMazeFrame extends JFrame implements ActionListener
 {
     private ArrowMazePanel mazePanel;
-    private JButton resetButton, executeButton;
+    private JButton resetButton, find1PathButton, findAllPathsButton;
+    private JLabel statusLabel;
 
 
-    public ArrowMazeFrame()
-    {
+    public ArrowMazeFrame() {
         super("ArrowMaze");
-        setSize((ArrowCell.LEFT_MARGIN*2+ArrowCell.CELL_SIZE*ArrowMazePanel.NUM_COLS),
-                ArrowCell.TOP_MARGIN*2+ArrowCell.CELL_SIZE*ArrowMazePanel.NUM_ROWS+80);
+        setSize((ArrowCell.LEFT_MARGIN * 2 + ArrowCell.CELL_SIZE * ArrowMazePanel.NUM_COLS),
+                ArrowCell.TOP_MARGIN * 2 + ArrowCell.CELL_SIZE * ArrowMazePanel.NUM_ROWS + 80);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(new BorderLayout());
@@ -23,14 +23,23 @@ public class ArrowMazeFrame extends JFrame implements ActionListener
         getContentPane().add(mazePanel, BorderLayout.CENTER);
 
         // now layout the bottom panel with the buttons
-        JPanel bottomPanel = new JPanel();
+        Box bottomPanel = Box.createVerticalBox();
+        JPanel bottomButtonPanel = new JPanel();
         resetButton = new JButton("Reset");
-        executeButton =new JButton("Execute");
+        find1PathButton = new JButton("Find1Path");
+        findAllPathsButton = new JButton("Find All Paths");
         resetButton.addActionListener(this);
-        executeButton.addActionListener(this);
-        bottomPanel.add(resetButton);
-        bottomPanel.add(executeButton);
+        find1PathButton.addActionListener(this);
+        findAllPathsButton.addActionListener(this);
+        bottomButtonPanel.add(resetButton);
+        bottomButtonPanel.add(find1PathButton);
+        bottomButtonPanel.add(findAllPathsButton);
+
+        statusLabel = new JLabel("Started.");
+        bottomPanel.add(bottomButtonPanel);
+        bottomPanel.add(statusLabel);
         getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+
     }
 
     @Override
@@ -40,11 +49,22 @@ public class ArrowMazeFrame extends JFrame implements ActionListener
         {
             System.out.println("Reset pressed");
             mazePanel.resetField();
+            statusLabel.setText("Reset.");
         }
-        if (aEvt.getSource()==executeButton)
+        if (aEvt.getSource()==find1PathButton)
+        {
+            System.out.println("Find 1 path pressed.");
+            mazePanel.find1Path();
+            statusLabel.setText((int)(100*mazePanel.getPercentageColored())+"% colored");
+        }
+        if (aEvt.getSource()== findAllPathsButton)
         {
             System.out.println("Execute pressed");
-            mazePanel.execute();
+            mazePanel.findAllPaths();
+            if (mazePanel.noBlackCellsRemain())
+                statusLabel.setText("Confirmed all cells colored.");
+            else
+                statusLabel.setText("Error. Not all cells colored.");
         }
     }
 }
